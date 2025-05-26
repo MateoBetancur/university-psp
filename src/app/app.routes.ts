42bb47209fc7fa9d77bb927040c7643d827ed603
MateoBetancur <mateo.ech.bet@gmail.com>
2025-05-26 06:20:19 -0500

    feat(auth): add authentication guard to protect private routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/layout/layout.component';
+import { AuthGuard } from './core/auth/auth.guard';
 
 export const routes: Routes = [
     {
         path: '',
         component: LayoutComponent,
+        canActivate: [AuthGuard],
         children: [
             // {
             //     path: '',
